b2c00f0f9b66e2fa3a9054e10527ce291366465c
zamzammmm <zamzamzuba2017@gmail.com>
2020-02-26 11:44:42 +0700

    Auth Bootstrap

--- resources/views/dosen/index.blade.php
+++ resources/views/dosen/index.blade.php
@@ -3,6 +3,11 @@
 <div class="Container">
     <div class="row justify-content-center">
         <div class="col-md-10">
+            @if(session('message'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('message')}}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">
                     Data Dosen
@@ -29,7 +34,13 @@
                                     <td>{{$data->nama}}</td>
                                     <td>{{$data->nipd}}</td>
                                     <td>
-                                        Lihat | Edit | Hapus
+                                        <form action="{{route('dosen.destroy',$data->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <a href="{{route('dosen.show',$data->id)}}" class="btn btn-warning">Lihat</a> |
+                                            <a href="{{route('dosen.edit',$data->id)}}" class="btn btn-primary">Edit</a> |
+                                            <button type="submit" onclick="return confirm('Apakah Anda Yakin ?')" class="btn btn-danger">Hapus</button>
+                                        </form>
                                     </td>
                                 </tr>
                                 @endforeach
